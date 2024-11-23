bbec1b5325f37be50b72047e08f7133b118f0b1a
ArvindUpreti <arvindupreti405@gmail.com>
2024-11-23 22:12:29 +0530

    Charts Added

--- dashboard.py
+++ dashboard.py
@@ -20,6 +20,8 @@ pages=['Overall Analysis','Startups Analyis','Invester Analysis']
 btn1= st.sidebar.selectbox('Select Your Choice',pages)
 total=str(round(df.Funding_Amount_in_cr.sum(),2)) + " Cr"
 max_=str(round(df['Funding_Amount_in_cr'].max(),2)) +' Cr'
+top = df.groupby('Industry_Vertical')['Funding_Amount_in_cr'].sum().sort_values(ascending =False).head(3)
+df_ = pd.DataFrame(top)
 df_sorted = df.sort_values(by='Funding_Amount_in_cr', ascending=False).head(10)
 # selected_columns = df_sorted  
 if btn1==pages[0]:
@@ -35,6 +37,4 @@ if btn1==pages[1]:
 if btn1==pages[2]:
     btn2= st.sidebar.selectbox('Choose your Startup',df['Invester_name'].unique())
     st.metric(label='Total Investment',value=str((df.groupby('Invester_name')['Funding_Amount_in_cr'].sum().loc[btn2])) +' Crs ')
-    top = df.groupby('Industry_Vertical')['Funding_Amount_in_cr'].sum().sort_values(ascending =False).head(3)
-    
-    st.bar_chart(top)
+    st.bar_chart(df_)
